be3d42ab974f9018ef626b28ed852f0c4d6a8894
Isaac Carrillo <carrilloisaac824@gmail.com>
2025-11-05 20:16:08 -0500

    Se agrego la pagina de nosotros

---
+++ templates/Pages/nosotros.php
@@ -0,0 +1,59 @@
+<?php
+$this->disableAutoLayout();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Nosotos</title>
+</head>
+<body>
+    <header>
+        <nav class="menu_principal"> <!--menu principal -->
+            <a href="/index.html">
+                <img src="/assets\img\logo\logo_miro.webp" alt="Logo de Arroz miro">
+            </a>
+            <ul>
+                <li><a href="../index.html">Inicio</a></li>
+                <li><a href="nosotros.html">nosotros</a></li>
+                <li><a href="productos.html">Productos</a></li>
+                <li><a href="#">Noticias</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <h1>¿Quienes somos?</h1>
+
+    <footer>
+        <section class="footer-info">
+            <h4>Ubicación: </h4>
+            <address>
+                Vía Panamericana, San Pablo Viejo, Chiriquí.
+                Tel: +507 775-6532
+                Email: info@arrozmiro.com
+            </address>
+        </section>
+        <section class="footer-social">
+            <h4>Síguenos</h4>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/tu_pagina" target="_blank">
+                            <img src="../assets/img/icons/facebook.png" alt="Facebook">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/tu_pagina" target="_blank">
+                            <img src="../assets/img/icons/instagram.png" alt="Instagram">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+        <p>&copy; 2025 Arroz Miró. Todos los derechos reservados.</p>
+    </footer>   
+</body>
+</html>
