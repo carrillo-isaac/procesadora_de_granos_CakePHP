8e60a5b0838b00b77abf24407bbd1729dc0135b7
Isaac Carrillo <carrilloisaac824@gmail.com>
2025-11-16 11:01:28 -0500

    Iniciando el proceso de Authentication en cakePÂHP

--- src/Model/Entity/Usuario.php
+++ src/Model/Entity/Usuario.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
-
+use Authentication\PasswordHasher\DefaultPasswordHasher;
 /**
  * Usuario Entity
  *
@@ -20,13 +20,7 @@ use Cake\ORM\Entity;
  */
 class Usuario extends Entity
 {
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
+    /** 
      * @var array<string, bool>
      */
     protected array $_accessible = [
@@ -38,4 +32,11 @@ class Usuario extends Entity
         'carrito' => true,
         'facturas' => true,
     ];
+    protected function _setPassword(string $password) : ?string
+    {
+        if (strlen($password) > 0) {
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+        return null;
+    }
 }
