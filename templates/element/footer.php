ecf1f5e8e0fa9eacf1f20c03de706097f35b494f
Isaac Carrillo <carrilloisaac824@gmail.com>
2025-11-20 20:25:55 -0500

    Se crearon los elementos header y footer para pasarlos al layout default

---
+++ templates/element/footer.php
@@ -0,0 +1,28 @@
+<footer>
+        <section class="footer-info">
+            <h4>Ubicación: </h4>
+            <address>
+                Vía Panamericana, San Pablo Viejo, Chiriquí.
+                Tel: +507 775-6532
+                Email: info@arrozmiro.com
+            </address>
+        </section>
+        <section class="footer-social">
+            <h4>Síguenos</h4>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="https://www.facebook.com/tu_pagina" target="_blank">
+                            <img src="/img/icons/facebook.png" alt="Facebook">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/tu_pagina" target="_blank">
+                            <img src="/img/icons/instagram.png" alt="Instagram">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+        <p>&copy; 2025 Arroz Miró. Todos los derechos reservados.</p>
+    </footer>
---
+++ templates/element/header.php
@@ -0,0 +1,41 @@
+<header class="main-header">
+    <nav class="menu-principal">
+        <a href="/pages/home">
+            <img src="/img/logo/logo_miro.webp" alt="Logo de Arroz miro">
+        </a>
+        <ul class="menu-links">
+            <li><a href="/pages/home">Inicio</a></li>
+            <li><a href="/pages/nosotros">Nosotros</a></li>
+            <li><a href="/pages/productos_static">Productos</a></li>
+
+            <!-- Carrito -->
+            <li>
+                <a href="/carrito" class="icon-btn carrito">
+                    <i class="bi bi-cart3"></i>
+
+                    <?php 
+                        $carritoCount = isset($_SESSION['Carrito'])
+                            ? array_sum(array_column($_SESSION['Carrito'], 'cantidad'))
+                            : 0;
+                    ?>
+                    <?php if ($carritoCount > 0): ?>
+                        <span class="badge"><?= $carritoCount ?></span>
+                    <?php endif; ?>
+                </a>
+            </li>
+
+            <!-- Icono usuario -->
+            <li>
+                <?php if (isset($_SESSION['Auth']['User'])): ?>
+                    <a href="/usuarios/logout" class="icon-btn logout">
+                        <i class="bi bi-box-arrow-right"></i>
+                    </a>
+                <?php else: ?>
+                    <a href="/usuarios/login" class="icon-btn login">
+                        <i class="bi bi-person-circle"></i>
+                    </a>
+                <?php endif; ?>
+            </li>
+        </ul>
+    </nav>
+</header>
