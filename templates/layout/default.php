d26fadee5b6712e77a4d82a315fb7ab92ae650b8
jeireM <sgrupomusical@gmail.com>
2025-12-03 12:03:48 -0500

    Actualizo home con nuevo footer y estilos

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -5,22 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title><?= $this->fetch('title') ?></title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
 
-    <?= $this->Html->css(['style', 'nosotros', 'modal', 'header','footer','galeria']) ?>
-    <?= $this->fetch('/css') ?>
+    <!-- Íconos de Bootstrap -->
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+
+    <!-- Tus CSS en webroot/css -->
+    <?= $this->Html->css([
+        'style',   
+        'header',   
+        'nosotros',  
+        'modal',     
+        'footer',   
+        'galeria'  
+    ]) ?>
+
+    <!-- CSS extra que quiera inyectar alguna vista -->
+    <?= $this->fetch('css') ?>
 </head>
 
 <body>
 
+    <!-- HEADER COMÚN -->
     <?= $this->element('header') ?>
 
+    <!-- CONTENIDO DE CADA PÁGINA -->
     <main class="container">
         <?= $this->fetch('content') ?>
     </main>
 
+    <!-- FOOTER COMÚN -->
     <?= $this->element('footer') ?>
 
+    <!-- JS extra que inyecten las vistas -->
     <?= $this->fetch('script') ?>
 </body>
 </html>
