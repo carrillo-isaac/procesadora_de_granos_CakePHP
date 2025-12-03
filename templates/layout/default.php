c2693d85bcc746b322f21a31ce4a2d920022f762
Isaac Carrillo <carrilloisaac824@gmail.com>
2025-12-03 17:36:02 -0500

    ajsutustes en el archivo default, se arreglo el bug de letras sin sentido

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -7,27 +7,8 @@
 
     <title><?= $this->fetch('title') ?></title>
 
-<<<<<<< HEAD
     <?= $this->Html->css(['style', 'nosotros','carrusel-productos', 'modal', 'header','footer','galeria']) ?>
     <?= $this->fetch('/css') ?>
-=======
-    <!-- Íconos de Bootstrap -->
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-
-    <!-- Tus CSS en webroot/css -->
-    <?= $this->Html->css([
-        'style',
-        'header',
-        'nosotros',
-        'modal',
-        'footer',
-        'galeria'
-    ]) ?>
-
-    <!-- CSS extra que quiera inyectar alguna vista -->
-    <?= $this->fetch('css') ?>
->>>>>>> b8c02c9c694d90e6b3729700e5fc17a43ac9ba8c
 </head>
 
 <body>
@@ -42,13 +23,6 @@
 
     <!-- FOOTER COMÚN -->
     <?= $this->element('footer') ?>
-
-<<<<<<< HEAD
-    <?= $this->fetch('modal','productos','carrusel') ?>
-=======
-    <!-- JS extra que inyecten las vistas -->
-    <?= $this->fetch('script') ?>
->>>>>>> b8c02c9c694d90e6b3729700e5fc17a43ac9ba8c
 </body>
 
 </html>
