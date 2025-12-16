c81b5eb58da34cf34ea203f2c7192cbc31aa2ffa
Isaac Carrillo <carrilloisaac824@gmail.com>
2025-12-16 15:52:26 -0500

    prueba para layout de usuarios

---
+++ templates/element/Admin/crud-layout.php
@@ -0,0 +1,9 @@
+<div class="crud-container">
+    <aside class="crud-sidebar">
+        <?= $this->fetch('sidebar') ?>
+    </aside>
+
+    <section class="crud-content">
+        <?= $this->fetch('content') ?>
+    </section>
+</div>
